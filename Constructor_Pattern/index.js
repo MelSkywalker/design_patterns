b4aa6d9b5e10342329d3f90046d9b4beb02daa44
Developer <developer@IM-12-0269.local>
2019-10-09 12:04:14 -0500

    adds constructor pattern example

---
+++ Constructor_Pattern/index.js
@@ -0,0 +1,23 @@
+function Pokemon(name, type){
+    this.name = name;
+    this.type = type;
+}
+
+Pokemon.prototype.attack = function (){
+    switch(this.type) {
+        case 'fire':
+            return this.name + " used ember!";
+            break;
+        case 'water':
+            return this.name + " used hydro pump!";
+            break;
+        default:
+            return this.name + " cannot attack!."
+    }
+}
+
+const charmander = new Pokemon("Charmander", "fire");
+const squirtle = new Pokemon("Squirtle", "water");
+const bulbasaur = new Pokemon("Bulbasaur", "grass");
+
+console.log(bulbasaur.attack());
