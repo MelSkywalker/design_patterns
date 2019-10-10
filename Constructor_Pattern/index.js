a109262de4e3a3aa9196b50dd27ca767de873d7b
MEL <melisa.quc@gmail.com>
2019-10-10 16:00:46 -0500

    failed tests

--- Constructor_Pattern/index.js
+++ Constructor_Pattern/index.js
@@ -11,6 +11,54 @@ Pokemon.prototype.attack = function (){
         case 'water':
             return this.name + " used hydro pump!";
             break;
+        case 'grass':
+            return this.name + " used energy ball!";
+            break;
+        case 'bug':
+            return this.name + " used silver wind!";
+            break;
+        case 'dark':
+            return this.name + " used bite!";
+            break;
+        case 'dragon':
+            return this.name + " used dragon dance!";
+            break;
+        case 'electric':
+            return this.name + " used electro ball!";
+            break;
+        case 'fairy':
+            return this.name + " used charm!";
+            break;
+        case 'fighting':
+            return this.name + " used jump kick!";
+            break;
+        case 'flying':
+            return this.name + " used gust!";
+            break;
+        case 'ghost':
+            return this.name + " used curse!";
+            break;
+        case 'ground':
+            return this.name + " used dig!";
+            break;
+        case 'ice':
+            return this.name + " used avalanche!";
+            break;
+        case 'normal':
+            return this.name + " used growl!";
+            break;
+        case 'poison':
+            return this.name + " used acid!";
+            break;
+        case 'psychic':
+            return this.name + " used hypnosis!";
+            break;
+        case 'rock':
+            return this.name + " used sandstorm!";
+            break;
+        case 'steel':
+            return this.name + " used gyro ball!";
+            break;
         default:
             return this.name + " cannot attack!"
     }
@@ -19,6 +67,7 @@ Pokemon.prototype.attack = function (){
 const charmander = new Pokemon("Charmander", "fire");
 const squirtle = new Pokemon("Squirtle", "water");
 const bulbasaur = new Pokemon("Bulbasaur", "grass");
+const unknown = new Pokemon("Unknown", "notype");
 
 console.log(bulbasaur.attack());
 
--- test/test_constructorPattern.js
+++ test/test_constructorPattern.js
@@ -1,19 +1,17 @@
-var pokemon = require('../Constructor_Pattern/index');
+var Pokemon = require('../Constructor_Pattern/index');
 var expect = require('chai').expect;
 
-
-
-// describe('#Pokemon()', function(){
+describe('#Pokemon()', function(){
     
-//     context('with no valid type', function(){
-//         it('should return your pokemon cannot attack', function(){
-//             expect(pokemon('pokemon', 'not-valid-type')).to.equal('pokemon cannot attack!')
-//         })
-//     })
+    context('with no valid type', function(){
+        it('should return your pokemon cannot attack', function(){
+            expect(Pokemon('pokemon', 'not-valid-type').attack()).to.equal('pokemon cannot attack!')
+        })
+    })
 
-//     context('with valid type', function(){
-//         it('should return a pokemon attacking', function(){
-//             expect(pokemon('charmeleon', 'fire')).to.equal('charmeleon used ember!')
-//         })
-//     })
-// })
+    context('with valid type', function(){
+        it('should return a pokemon attacking', function(){
+            expect(Pokemon('charmeleon', 'fire').attack()).to.equal('charmeleon used ember!')
+        })
+    })
+})
